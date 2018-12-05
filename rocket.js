19a9c40a02cba98856a97cf235c4408f2fa2214e
Matthew Fine <matthewfine@Matthews-MacBook-Pro-2.local>
2018-12-05 14:20:36 -0500

    Add launch method to rocket.js

--- rocket.js
+++ rocket.js
@@ -1,6 +1,10 @@
 var Rocket = function (color, size) {
   this.color = color;
   this.size = size;
+
+  this.launch = funciton () {
+    console.log('3,2,1, blastoff!')
+  }
 }
 
 module.exports = Rocket;
